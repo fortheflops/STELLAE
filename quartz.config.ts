302d44b54278eeddeb79cc3c651abbb01e27c99c
Sabastian Benavidez <141684482+fortheflops@users.noreply.github.com>
2025-07-27 19:03:28 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -88,21 +88,7 @@ const config: QuartzConfig = {
       Plugin.Assets(),
       Plugin.Static(),
       Plugin.NotFoundPage(),
-    ],
-  },
-}
-import { CustomOgImages } from "./quartz/plugins/emitters/ogImage"
- 
-const config: QuartzConfig = {
-  plugins: {
-    emitters: [
-      CustomOgImages({
-        colorScheme: "lightMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
-        width: 1200, // width to generate with (in pixels)
-        height: 630, // height to generate with (in pixels)
-        excludeRoot: false, // wether to exclude "/" index path to be excluded from auto generated images (false = use auto, true = use default og image)
-        imageStructure: defaultImage, // custom image component to use
-      }),
+      Plugin.CustomOgImages(),
     ],
   },
 }
