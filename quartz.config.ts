0b65ceabb2795c655c687257983f43cd9e175e90
Sabastian Benavidez <141684482+fortheflops@users.noreply.github.com>
2025-09-04 18:13:34 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,6 +1,6 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
-
+import { byDateAndAlphabetical } from "./quartz/components/PageList"
 /**
  * Quartz 4 Configuration
  *
